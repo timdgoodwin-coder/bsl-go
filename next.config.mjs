3c16325ee07c927a8fe285fa0b135e5383d641f5
timdgoodwin-coder <tim@leangreens.com>
2026-04-03 15:52:27 +0100

    feat: rename routes listicle-1>/brain, listicle-2>/age with permanent redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/listicle-1',
+        destination: '/brain',
+        permanent: true,
+      },
+      {
+        source: '/listicle-2',
+        destination: '/age',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
